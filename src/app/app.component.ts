657e79866a82153a1e830047e262bf0d0e2f406f
richaparikh15 <46055833+richaparikh15@users.noreply.github.com>
2024-06-21 16:00:06 -0400

    reverted gtm script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,24 +47,27 @@ export class AppComponent implements OnInit{
   }
 
   loadGTM() {
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=GTM-5JGL95PG`;
-    script.setAttribute('nonce', this.nonce); // Set nonce attribute
-    script.setAttribute('id','gtmScript');
-    script.async = true;
-    script.onload = () => {
-      // Initialize gtag function once script is loaded
-      window['dataLayer'] = window['dataLayer'] || [];
-      function gtag(...args: any[]) {
-        window['dataLayer'].push(arguments);
-      }
+    // const script = document.createElement('script');
+    // script.src = `https://www.googletagmanager.com/gtag/js?id=GTM-5JGL95PG`;
+    // script.setAttribute('nonce', this.nonce); // Set nonce attribute
+    // script.setAttribute('id','gtmScript');
+    // script.async = true;
+    // script.onload = () => {
+    //   // Initialize gtag function once script is loaded
+    //   window['dataLayer'] = window['dataLayer'] || [];
+    //   function gtag(...args: any[]) {
+    //     window['dataLayer'].push(arguments);
+    //   }
 
-      // Initialize gtag and configure Google Analytics
-      gtag('js', new Date());
-      gtag('config', 'GTM-5JGL95PG'); // Replace with your Google Analytics Measurement ID
-    };
+    //   // Initialize gtag and configure Google Analytics
+    //   gtag('js', new Date());
+    //   gtag('config', 'GTM-5JGL95PG'); // Replace with your Google Analytics Measurement ID
+    // };
 
-    // Append script to the head of the document
-    document.head.appendChild(script);
+    // // Append script to the head of the document
+    // document.head.appendChild(script);
+
+    var nonceId = document.getElementById('gtmScript');
+    
   }
 }
